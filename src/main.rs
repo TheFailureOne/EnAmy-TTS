eb0d3141d5c750908d2ad23aa6317fac0d144775
Ofelia sillymabs Surname <sillymabs@proton.me>
2025-02-06 17:48:18 +0100

    Completed the UI user prompt by adding multiline functionality, only minor adjustments left to do

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,29 @@ slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+    const STARTING_HORIZONTAL_LIMIT: f32 = 1320.0;
+    let mut horizontal_limit = STARTING_HORIZONTAL_LIMIT;
+    let mut horizontal_width = ui.get_rectWidth();
 
     ui.set_rectWidth(30.0);
     ui.set_rectX(235.0);
+    ui.set_rectHeight(90.0);
     ui.on_editedText({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            ui.set_rectWidth(30.0 + (30.0 * ui.get_textBox().len() as f32));
+            horizontal_width = 30.0 + (30.0 * ui.get_textBox().len() as f32);
+            if horizontal_width <= STARTING_HORIZONTAL_LIMIT {
+                ui.set_rectWidth(horizontal_width);
+            }
+            else if horizontal_width >= horizontal_limit {
+                ui.set_rectHeight(ui.get_rectHeight() + 90.0);
+                horizontal_limit += STARTING_HORIZONTAL_LIMIT;
+            }
+            if horizontal_width <= horizontal_limit - STARTING_HORIZONTAL_LIMIT{
+                ui.set_rectHeight(ui.get_rectHeight() - 90.0);
+                horizontal_limit -= STARTING_HORIZONTAL_LIMIT;
+            }
             ui.set_rectX(235.0 - (15.0 * ui.get_textBox().len() as f32));
         }
     });
