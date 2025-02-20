307290abde2c65248f23133e9ac6c5061c413902
Ofelia sillymabs Surname <sillymabs@proton.me>
2025-02-20 18:03:48 +0100

    Added global hotkeys Now it can stay on in the background and get called via Ctrl+Enter instead Close the window with Esc Close the whole program by writting cmd::quit

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,11 @@ use curl::easy::Easy;
 use curl::easy::List;
 use serde::{Deserialize, Serialize};
 // use slint::platform::{Key, WindowEvent};
-use slint::ToSharedString;
+use slint::{Global, ToSharedString};
 use regex::Regex;
 use dirs::config_dir;
+use global_hotkey::{GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}, GlobalHotKeyEvent};
+
 
 slint::include_modules!();
 
@@ -26,6 +28,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let _input_active = BufWriter::new(File::create("/tmp/enamy/inputActive").unwrap());
     let site = "http://192.168.122.193:5000/api/HotelBooking/CreateVoice";
     const STARTING_HORIZONTAL_LIMIT: f32 = 1320.0;
+    let manager = GlobalHotKeyManager::new().unwrap();
+    let hotkey = HotKey::new(Some(Modifiers::CONTROL), Code::Enter);
     
 
     while !fs::metadata("/tmp/enamy/lock").is_err() {
@@ -64,38 +68,51 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let ui_handle = ui.as_weak();
                 move || {
                     let ui = ui_handle.unwrap();
-                    let c = ui.get_textBox().clone();
-                    let a = replace_with_config(c.to_string());
-                    
-                    let text = format!("{{ \"line\": \"{a}\" }}");
-                    let mut data = text.as_bytes();
-                    let mut easy = Easy::new();
-                    easy.url(site).expect("Failed to set URL");
-                    easy.post(true).unwrap();
-                    easy.post_field_size(data.len() as u64).unwrap();
-                    let mut header = List::new();
-                    header.append("Content-Type: application/json").expect("Failed to add header");
-                    easy.http_headers(header).unwrap();
-                    let mut file = BufWriter::new(File::create("/tmp/enamy/response.wav").unwrap());
+                    if ui.get_textBox() == "cmd::quit" {
+                        Command::new("rm")
+                        .arg("/tmp/enamy/lock").spawn().unwrap();
+                        println!("lock removed");
 
-                    easy.write_function(move |data| {
-                        file.write_all(data).unwrap();
-                        Ok(data.len())
-                    }).unwrap();
+                        Command::new("rm")
+                        .arg("/tmp/enamy/inputActive").spawn().unwrap();
+                        println!("inputActive removed");
+
+                        ui.window().hide().unwrap();
+                    }
+                    else {
+                        let c = ui.get_textBox().clone();
+                        let a = replace_with_config(c.to_string());
+                        
+                        let text = format!("{{ \"line\": \"{a}\" }}");
+                        let mut data = text.as_bytes();
+                        let mut easy = Easy::new();
+                        easy.url(site).expect("Failed to set URL");
+                        easy.post(true).unwrap();
+                        easy.post_field_size(data.len() as u64).unwrap();
+                        let mut header = List::new();
+                        header.append("Content-Type: application/json").expect("Failed to add header");
+                        easy.http_headers(header).unwrap();
+                        let mut file = BufWriter::new(File::create("/tmp/enamy/response.wav").unwrap());
+
+                        easy.write_function(move |data| {
+                            file.write_all(data).unwrap();
+                            Ok(data.len())
+                        }).unwrap();
 
 
-                    let mut transfer = easy.transfer();
-                    transfer.read_function(|buf| {
-                        Ok(data.read(buf).unwrap_or(0))
-                    }).unwrap();
-                    transfer.perform().unwrap();
+                        let mut transfer = easy.transfer();
+                        transfer.read_function(|buf| {
+                            Ok(data.read(buf).unwrap_or(0))
+                        }).unwrap();
+                        transfer.perform().unwrap();
 
-                    Command::new("play")
-                        .arg("/tmp/enamy/response.wav")
-                        .spawn().unwrap();
+                        Command::new("play")
+                            .arg("/tmp/enamy/response.wav")
+                            .spawn().unwrap();
 
-                    ui.set_textBox("".to_shared_string());
-                    ui.invoke_editedText();
+                        ui.set_textBox("".to_shared_string());
+                        ui.invoke_editedText();
+                    }
 
                     //weak.unwrap().window().hide().unwrap();
 
@@ -104,6 +121,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             ui.on_closeWindow({
                 let ui_handle = ui.as_weak();
+                // let test_handle = &test;
                 move || {
                     let ui = ui_handle.unwrap();
                     if ui.get_keyDown().text == "\u{1b}"{
@@ -123,9 +141,13 @@ fn main() -> Result<(), Box<dyn Error>> {
             ui.run()?;
         }
         else{
-            
+            manager.register(hotkey);
+
+            if let Ok(event) = GlobalHotKeyEvent::receiver().try_recv(){
+                println!("{:?}", event);
+                BufWriter::new(File::create("/tmp/enamy/inputActive").unwrap());
+            }
         }
-        
     }
     Ok(())
 }
